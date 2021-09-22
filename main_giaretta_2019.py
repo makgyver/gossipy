c71ffe879b98e2213e22083f3e096a61fc7b34ab
Mirko <mak1788@gmail.com>
2021-09-22 15:35:39 +0200

    Fixed the mains according to the last changes in gossipy.simul

--- main_giaretta_2019.py
+++ main_giaretta_2019.py
@@ -6,27 +6,28 @@ from gossipy.model.handler import PegasosHandler
 from gossipy.model.nn import Pegasos
 from gossipy.data import load_classification_dataset, DataDispatcher
 from gossipy.data.handler import ClassificationDataHandler
-from gossipy.simul import repeat_simulation
+from gossipy.simul import GossipSimulator, repeat_simulation
 
 
 set_seed(42)
 X, y = load_classification_dataset("spambase", as_tensor=True)
 y = 2*y - 1 #convert 0/1 labels to -1/1
 data_handler = ClassificationDataHandler(X, y, test_size=.1)
-
 topology = nx.to_numpy_matrix(random_tree(data_handler.size()))
 
-res = repeat_simulation(data_dispatcher=DataDispatcher(data_handler, eval_on_user=False),
-                        round_delta=100,
-                        protocol=AntiEntropyProtocol.PUSH, 
-                        gossip_node_class=GossipNode,
-                        model_handler_class=PegasosHandler,
-                        model_handler_params={"net" : Pegasos(data_handler.size(1)),
-                                              "lam" : .01,
-                                              "create_model_mode" : CreateModelMode.MERGE_UPDATE},
-                        topology=topology,
+simulator = GossipSimulator(data_dispatcher=DataDispatcher(data_handler, eval_on_user=False),
+                            delta=100,
+                            protocol=AntiEntropyProtocol.PUSH,
+                            gossip_node_class=GossipNode,
+                            model_handler_class=PegasosHandler,
+                            model_handler_params={"net" : Pegasos(data_handler.size(1)),
+                                                "lam" : .01,
+                                                "create_model_mode" : CreateModelMode.MERGE_UPDATE},
+                            topology=topology,
+                            round_synced=False)
+
+res = repeat_simulation(gossip_simulator=simulator,
                         n_rounds=100,
                         repetitions=1,
-                        round_synced=False,
                         verbose=True)
 
--- main_ormandi_2013.py
+++ main_ormandi_2013.py
@@ -4,7 +4,7 @@ from gossipy.model.handler import PegasosHandler
 from gossipy.model.nn import Pegasos
 from gossipy.data import load_classification_dataset, DataDispatcher
 from gossipy.data.handler import ClassificationDataHandler
-from gossipy.simul import repeat_simulation
+from gossipy.simul import GossipSimulator, repeat_simulation
 
 
 set_seed(42)
@@ -12,17 +12,20 @@ X, y = load_classification_dataset("spambase", as_tensor=True)
 y = 2*y - 1 #convert 0/1 labels to -1/1
 data_handler = ClassificationDataHandler(X, y, test_size=.1)
 
-res = repeat_simulation(data_dispatcher=DataDispatcher(data_handler, eval_on_user=False),
-                        round_delta=100,
-                        protocol=AntiEntropyProtocol.PUSH, 
-                        gossip_node_class=GossipNode,
-                        model_handler_class=PegasosHandler,
-                        model_handler_params={"net" : Pegasos(data_handler.size(1)),
-                                              "lam" : .01,
-                                              "create_model_mode" : CreateModelMode.MERGE_UPDATE},
-                        topology=None,
+simulator = GossipSimulator(data_dispatcher=DataDispatcher(data_handler, eval_on_user=False),
+                            delta=100,
+                            protocol=AntiEntropyProtocol.PUSH,
+                            gossip_node_class=GossipNode,
+                            model_handler_class=PegasosHandler,
+                            model_handler_params={"net" : Pegasos(data_handler.size(1)),
+                                                "lam" : .01,
+                                                "create_model_mode" : CreateModelMode.MERGE_UPDATE},
+                            topology=None,
+                            round_synced=False)
+
+res = repeat_simulation(gossip_simulator=simulator,
                         n_rounds=100,
                         repetitions=1,
-                        round_synced=False,
                         verbose=True)
+
 
