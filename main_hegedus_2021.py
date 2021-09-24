702266b4641866b73410416dc49a5e71d5242958
Mirko <mak1788@gmail.com>
2021-09-24 15:07:39 +0200

    Update main_hegedus_2021.py

--- main_hegedus_2021.py
+++ main_hegedus_2021.py
@@ -19,11 +19,12 @@ dispatcher = DataDispatcher(data_handler, n=100, eval_on_user=False)
 topology = to_numpy_matrix(random_regular_graph(20, 100, seed=42))
 net = LogisticRegression(data_handler.Xtr.shape[1], 2)
 
-sim = TokenizedGossipSimulator(
+#sim = TokenizedGossipSimulator(
+sim = GossipSimulator(
     data_dispatcher=dispatcher,
-    token_account_class=RandomizedTokenAccount,
-    token_account_params={"C": 10, "A": 10},
-    utility_fun=lambda mh1, mh2: 1,
+    #token_account_class=RandomizedTokenAccount, #Coincides with the paper's setting
+    #token_account_params={"C": 20, "A": 10},
+    #utility_fun=lambda mh1, mh2: 1, #The utility function is always = 1 (i.e., utility is not used)
     delta=100,
     protocol=AntiEntropyProtocol.PUSH, 
     gossip_node_class=SamplingBasedNode,
@@ -32,21 +33,23 @@ sim = TokenizedGossipSimulator(
     #model_handler_class=PartitionedTMH,
     model_handler_params={
         "sample_size" : .25,
-        #"tm_partition": TorchModelPartition(net, 5),
+        #"tm_partition": TorchModelPartition(net, 4),
         "net" : net,
         "optimizer" : torch.optim.SGD,
         "l2_reg": .001,
         "criterion" : CrossEntropyLoss(),
         "learning_rate" : 1,
-        "create_model_mode" : CreateModelMode.UPDATE
+        "create_model_mode" : CreateModelMode.UPDATE #CreateModelMode.MERGE_UPDATE
     },
     topology=topology,
     delay=(0, 10),
+    online_prob=.2, #Approximates the average online rate of the STUNner's smartphone traces
+    #drop_prob=.1, #Simulate the possibility of message dropping
     sampling_eval=.1,
     round_synced=True
 )
 sim.init_nodes()
-evaluation, evaluation_user = sim.start(n_rounds=100)
+evaluation, evaluation_user = sim.start(n_rounds=1000)
 
 plot_evaluation([evaluation])
 plot_evaluation([evaluation_user])
